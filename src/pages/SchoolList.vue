4431c6b04ac40169a5b1e6deb246052cca73cbff
PageAberdeen <java_scr1pt@163.com>
2017-06-29 08:58:12 +0800

    addVue
    学校展示页面

---
+++ src/pages/SchoolList.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="schoolList">
+    <div class="schoolList-top">
+      <input type="text" class="search icon-search" placeholder="请输入预约学校名称">
+    </div>
+    <div class="schoolList-main">
+      <dl class="clearfix list-box">
+        <dt class="school-logo">
+          <img src="../assets/images/wtlogo.png" alt="">
+        </dt>
+        <dd class="schoolName">柳州市弯塘路小学</dd>
+        <dd class="schoolTitle">学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述           </dd>
+      </dl>
+      <dl class="clearfix list-box">
+        <dt class="school-logo">
+          <img src="../assets/images/wtlogo.png" alt="">
+        </dt>
+        <dd class="schoolName">柳州市弯塘路小学</dd>
+        <dd class="schoolTitle">学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述           </dd>
+      </dl>
+      <dl class="clearfix list-box">
+        <dt class="school-logo">
+          <img src="../assets/images/wtlogo.png" alt="">
+        </dt>
+        <dd class="schoolName">柳州市弯塘路小学</dd>
+        <dd class="schoolTitle">学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述           </dd>
+      </dl>
+      <dl class="clearfix list-box">
+        <dt class="school-logo">
+          <img src="../assets/images/wtlogo.png" alt="">
+        </dt>
+        <dd class="schoolName">柳州市弯塘路小学</dd>
+        <dd class="schoolTitle">学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述           </dd>
+      </dl>
+      <dl class="clearfix list-box">
+        <dt class="school-logo">
+          <img src="../assets/images/wtlogo.png" alt="">
+        </dt>
+        <dd class="schoolName">柳州市弯塘路小学</dd>
+        <dd class="schoolTitle">学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述           </dd>
+      </dl>
+      <dl class="clearfix list-box">
+        <dt class="school-logo">
+          <img src="../assets/images/wtlogo.png" alt="">
+        </dt>
+        <dd class="schoolName">柳州市弯塘路小学</dd>
+        <dd class="schoolTitle">学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述学校描述           </dd>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.schoolList
+  padding .4rem
+  background url(../assets/images/schoolList.jpg) no-repeat
+  background-size cover
+  &-top
+    position fixed
+    top: 0
+    left: 0
+    width: 100%
+    background-color rgba(0,0,0,.2)
+  &-main
+    margin-top 1rem
+    color #fff
+  
+  .search
+    margin .2rem auto
+    width 87%
+    border-radius .3rem
+    padding-left .8rem
+    background-color transparent
+  .list-box
+    overflow hidden
+    margin-bottom .3rem
+    border-top 1px solid #ddd
+    padding-top .2rem
+  .school-logo
+    float left
+    padding .1rem
+    margin-right .2rem
+    background-color rgba(255,255,255,.6)
+    border-radius .1rem
+  .schoolName
+    font-size .32rem
+  .schoolTitle
+    margin-top .26rem
+    overflow hidden
+    font-size .28rem
+    text-overflow ellipsis
+    display -webkit-box
+    -webkit-box-orient vertical
+    -webkit-line-clamp 2
+  vertical-align bottom
+</style>
